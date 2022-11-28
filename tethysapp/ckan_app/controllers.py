42728dd4d6bbf88a6dac11485bc1bd66f3039985
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-28 20:31:32 +0000

    Adding tarfile member sanitization to extractall()

--- tethysapp/ckan_app/controllers.py
+++ tethysapp/ckan_app/controllers.py
@@ -190,7 +190,26 @@ def plot(request, resource_id):
 
         # Extract the archive file
         with tarfile.open(file_name) as tar:
-            tar.extractall(user_workspace)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, user_workspace)
         
         # Search directory for files with the netCDF extension (.nc)
         netcdf_file_path = ''
